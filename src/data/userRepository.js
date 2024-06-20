8292eb9df921dd065bf49c7db100614835cfcc00
k08e24bryant <122170154+k08e24bryant@users.noreply.github.com>
2024-06-21 00:14:17 +0700

    Update userRepository.js

--- src/data/userRepository.js
+++ src/data/userRepository.js
@@ -1,9 +1,11 @@
-const db = require('../infrastructure/db');
+const db = require('../infrastructure/db').db;
 
 const userRepository = {
     findByUsername: (username, callback) => {
-        const sql = 'SELECT * FROM admins WHERE username = ?';
-        db.get(sql, [username], callback);
+        const sql = `SELECT * FROM admins WHERE username = ?`;
+        db.get(sql, [username], (err, row) => {
+            callback(err, row);
+        });
     }
 };
 
